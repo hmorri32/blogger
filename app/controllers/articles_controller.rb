ab16ccc66b04ea214b301b51ff93b96fcdad63e8
Hugh Morrison <hmorri32@gmail.com>
2017-10-14 13:41:37 -0600

    comments

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,10 +5,14 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+    @comment = Comment.new
+    @comment.article_id = @article.id
   end
 
   def new
     @article = Article.new
+    @comment = Comment.new
+    @comment.article_id = @article.id
   end
 
   def create
