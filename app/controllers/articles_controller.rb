ddb280b50d36cfc683f8e998ce2945f26ae57727
Hugh Morrison <hmorri32@gmail.com>
2017-10-14 18:38:25 -0600

    restructure article controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,45 +1,44 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:destroy, :edit, :show, :update]
+
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
     @comment = Comment.new
     @comment.article_id = @article.id
   end
 
   def new
     @article = Article.new
-    @comment = Comment.new
-    @comment.article_id = @article.id
   end
 
   def create
-    @article = Article.new(article_params)
-    @article.save
-    flash.notice = "Article '#{@article.title}' Created!"
+    @article = Article.create(article_params)
     redirect_to article_path(@article)
   end
 
   def edit
-    @article = Article.find(params[:id])
-  end
-
-  def destroy
-    Article.destroy(params[:id])
-    flash.notice = "Article Destroyed"
-    redirect_to articles_path
   end
 
   def update
-    @article = Article.find(params[:id])
     @article.update(article_params)
     flash.notice = "Article '#{@article.title}' Updated!"
     redirect_to article_path(@article)
   end
 
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
   private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
   def article_params
     params.require(:article).permit(:title, :body)
   end
